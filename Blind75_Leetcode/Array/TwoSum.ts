9a5ce1ec98830231f195f222d93d2b201cc68c3d
Ngoc Ha <ha69630@gmail.com>
2026-01-04 09:19:59 +0700

    Update Two Sum

---
+++ Blind75_Leetcode/Array/TwoSum.ts
@@ -0,0 +1,11 @@
+function TwoSum(nums: number[], target: number): number[] {
+    for (let i=0; i<nums.length; i++){
+        for (let j=i+1; j<nums.length; j++) {
+            if (nums[j] === target - nums[i]) {
+                return [i, j];
+            }
+        }
+    }
+    return [];
+};
+
--- Blind75_Leetcode/Array/twoSum.js
+++ Blind75_Leetcode/Array/twoSum.js
@@ -1,12 +1,27 @@
+// Cách 1 O(n^2)
 var twoSum = function(numbers, target) {
-    for (i=0; i<numbers.length; i++){
-        for (j=i+1; j<numbers.length; j++){
+    for (let i=0; i<numbers.length; i++){
+        for (let j=i+1; j<numbers.length; j++){
             if (numbers[i] + numbers[j] === target){
                 return [i, j];
-            } else {
-                console.log("Not found numbers equal target");
-            }
+            } 
         }
     }
+    return "Not found numbers equal target";
+}
+
+//Cách 2 O(n)
+var twoSum = function(numbers, target) {
+    let map = new Map();
+
+    for (let i = 0; i < numbers.length; i++) {
+        let need = target - numbers[i];
+
+        if (map.has(need)) {
+            return [map.get(need), i];
+        }
+        map.set(numbers[i], i);
+    }
+    return "Not found numbers equal target";
 }
 console.log(twoSum([2,7,11,15], 9));
