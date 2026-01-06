bad2d62a177f9851cca80a6c2c0cbfee1a13aab8
Ngoc Ha <ha69630@gmail.com>
2026-01-06 23:02:32 +0700

    More Solution for Problem Contains duplicate

--- Blind75_Leetcode/Array/Contains_Duplicate/ContainsDuplicate.ts
+++ Blind75_Leetcode/Array/Contains_Duplicate/ContainsDuplicate.ts
@@ -12,3 +12,43 @@ function ContainsDuplicate_BF(nums: number[]): boolean {
 }
 
 console.log(ContainsDuplicate_BF([1,2,3,1]));
+
+
+//O(n)
+//Solution: Sorting
+function ContainsDuplicate_Sort(nums: number[]): boolean {
+    nums.sort();
+    for (let i=0; i<nums.length; i++) {
+        if (nums[i] === nums[i-1]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+console.log(ContainsDuplicate_Sort([1,2,3,1]));
+
+
+//O(n)
+//Solution: Set
+function ContainsDuplicate_Set(nums: number[]): boolean {
+    const set = new Set();
+    for (const num of nums) {
+        if(set.has(num)) {
+            return true;
+        }
+        set.add(num);
+    }
+    return false;
+}
+
+console.log(ContainsDuplicate_Set([1,2,3,1]));
+
+
+//O(n)
+//Solution: Hash Set Length
+function ContainsDuplicate_HashSetLength(nums: number[]): boolean {
+    return new Set(nums).size < nums.length;
+}
+
+console.log(ContainsDuplicate_HashSetLength([1,2,3,1]));
--- Blind75_Leetcode/Array/Contains_Duplicate/containsDuplicate.js
+++ Blind75_Leetcode/Array/Contains_Duplicate/containsDuplicate.js
@@ -13,3 +13,39 @@ var containsDuplicate_BF = function(nums) {
 
 console.log(containsDuplicate_BF([1,2,3,1])); 
 
+
+//O(n) time complexity
+//Solution: Sorting
+var containsDuplicate_Sort = function(nums) {
+    nums.sort();
+    for (let i=0; i<nums.length; i++) {
+        if (nums[i] === nums[i-1]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+console.log(containsDuplicate_Sort([1,1,3,1,4,9,5,2]));
+
+
+//O(n) time complexity
+//Solution: HashSet
+var containsDuplicate_HS = function(nums) {
+    const set = new Set();
+    for (const num of nums) {
+        if (set.has(num)) return true;
+        set.add(num);
+    }
+    return false;
+}
+console.log(containsDuplicate_HS([1,1,3,1,4,9,5,2]));
+
+
+//O(n) time complexity
+//Solution: Hash Set Length
+var containsDuplicate_HashSetLength = function(nums) {
+    return new Set(nums).size < nums.length;
+}
+
+console.log(containsDuplicate_HashSetLength([1,1,3,1,4,9,5,2]));
