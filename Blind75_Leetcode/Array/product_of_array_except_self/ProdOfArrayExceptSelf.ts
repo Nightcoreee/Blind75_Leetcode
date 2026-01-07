81662ebedb322f96f7874559390de29b1198903b
Ngoc Ha <ha69630@gmail.com>
2026-01-08 00:29:02 +0700

    More solution Product of Array Except Self

--- Blind75_Leetcode/Array/product_of_array_except_self/ProdOfArrayExceptSelf.ts
+++ Blind75_Leetcode/Array/product_of_array_except_self/ProdOfArrayExceptSelf.ts
@@ -16,3 +16,55 @@ function ProdOfArrayExceptSelf_BF(nums: number[]): number[] {
 }
 
 console.log(ProdOfArrayExceptSelf_BF([1,2,3,4]));
+
+
+//O(n) 
+//Solution: Prefix and Suffix Product
+function ProdOfArrayExceptSelf_PrefixSuffix(nums: number[]): number[] {
+    let n = nums.length; 
+    const pref = new Array(n);
+    const suff = new Array(n);
+    const res = new Array(n);
+
+    pref[0] = 1;
+    suff[n-1] = 1;
+
+    for (let i=1; i<n; i++) {
+        //nums[0] tăng dần
+        pref[i] = nums[i-1] * pref[i-1];
+    }
+
+    for (let i=n-2; i>=0; i--) {
+        //nums[3] giảm dần
+        suff[i] = nums[i+1] * suff[i+1];
+    }
+
+    for (let i=0; i<n; i++) {
+        res[i] = pref[i] * suff[i];
+    }
+
+    return res;
+}
+
+console.log(ProdOfArrayExceptSelf_PrefixSuffix([1,2,3,4]));
+
+//O(n)
+//Solution: Optimized Prefix and Suffix Optimal
+function ProdOfArrayExceptSelf_Optimal(nums: number[]): number[] {
+    let n = nums.length;
+    const res = new Array(n).fill(1);
+
+    for (let i=1; i<n; i++) {
+        res[i] = nums[i-1] * res[i-1];
+    }
+
+    let postfix = 1;
+    for (let i=n-1; i>=0; i--) {
+        res[i] *= postfix;
+        postfix *= nums[i];
+    }
+    return res;
+}
+
+console.log(ProdOfArrayExceptSelf_Optimal([1,2,3,4]));
+
--- Blind75_Leetcode/Array/product_of_array_except_self/prodOfArrayExceptSelf.js
+++ Blind75_Leetcode/Array/product_of_array_except_self/prodOfArrayExceptSelf.js
@@ -16,3 +16,55 @@ var prodOfArrayExceptSelf_BF = function(nums) {
 }
 
 console.log(prodOfArrayExceptSelf_BF([1,2,3,4]));
+
+
+//O(n) time
+//Solution: Prefix and Suffix Product
+var prodOfArrayExceptSelf_PS = function(nums) {
+    let n = nums.length;
+    const pref = new Array(n);
+    const suff = new Array(n);
+    const res = new Array(n);
+    
+    pref[0] = 1;
+    suff[n-1] = 1;
+    
+    //Tích các phần tử bên trái của i
+    for (let i=1; i<n; i++) {
+        pref[i] = nums[i-1] * pref[i-1];
+    }
+
+    //Tích các phần tử bên phải của i
+    for (let i=n-2; i>=0; i--) {
+        suff[i] = nums[i+1] * suff[i+1];
+    }
+
+    //Kết quả là tích của prefix và suffix
+    for (let i=0; i<n; i++) {
+        res[i] = pref[i] * suff[i];
+    }
+
+    return res;
+}
+
+console.log(prodOfArrayExceptSelf_PS([1,2,3,4]));
+
+
+var prodOfArrayExceptSelf_Opt = function(nums) {
+    let n = nums.length;
+    const res =  new Array(n).fill(1);
+
+    for (let i=1; i<n; i++) {
+        res[i] = res[i-1] * nums[i-1];
+    }   
+
+    let postfix = 1;
+    for (let i=n-1; i>=0; i--) {
+        res[i] *= postfix;
+        postfix *= nums[i];
+    }
+
+    return res; 
+}
+
+console.log(prodOfArrayExceptSelf_Opt([1,2,3,4]));
