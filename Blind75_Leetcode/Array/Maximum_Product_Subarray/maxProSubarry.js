6214a7fbd367195091862a3f94307e8dcacc22b2
Hà Đào <94346501+Nightcoreee@users.noreply.github.com>
2026-02-13 23:57:43 +0700

    Solutions for Maximum Product Subarray

---
+++ Blind75_Leetcode/Array/Maximum_Product_Subarray/maxProSubarry.js
@@ -0,0 +1,36 @@
+//O(n^2)
+//Solution: Brute Force
+var maxProSubarray_BR = function(nums) {
+    let res = nums[0];
+
+    for (let i = 0; i < nums.length; i++) {
+        let cur = nums[i];
+        res = Math.max(res, cur);
+        for (let j = i + 1; j < nums.length; j++) {
+            cur *= nums[j];
+            res = Math.max(res, cur);
+        }
+    }
+    return res;
+}
+
+console.log(maxProSubarray_BR([2,3,-2,4]));
+
+
+//O(n)
+//Solution: Kadane's Algorithm
+var maxProSubarray_Kadane = function(nums) {
+    let res = numss[0];
+    let curMin = 1;
+    let curMax = 1;
+
+    for (const num of nums) {
+         const tmp = curMax * num;
+            curMax = Math.max(Math.max(num * curMax, num * curMin), num);
+            curMin = Math.min(Math.min(tmp, num * curMin), num);
+            res = Math.max(res, curMax);
+        }
+        return res;
+}
+
+console.log(maxProSubarray_Kadane([2,3,-2,4]));
