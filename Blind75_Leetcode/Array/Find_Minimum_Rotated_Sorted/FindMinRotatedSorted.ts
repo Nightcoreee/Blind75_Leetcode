68f63179b56e5eb6f141f73fdcfc60ad1ce2b37b
Hà Đào <94346501+Nightcoreee@users.noreply.github.com>
2026-02-17 18:43:12 +0700

    More solution for problem find minimum in rotated sorted array

--- Blind75_Leetcode/Array/Find_Minimum_Rotated_Sorted/FindMinRotatedSorted.ts
+++ Blind75_Leetcode/Array/Find_Minimum_Rotated_Sorted/FindMinRotatedSorted.ts
@@ -32,3 +32,23 @@ function FindMinRotatedSorted_BS(nums: number[]): number {
 }
 
 console.log(FindMinRotatedSorted_BS([3,4,5,1,2]));
+
+
+//O(logn)
+//Solution: Binary Search
+function FindMinRotatedSorted_BSLB(nums: number[]): number {
+    let l = 0;
+    let r = nums.length - 1;
+
+    while (l < r) {
+        let m = l + Math.floor((r - l)/2);
+        if (nums[m] < nums[r]){
+            r = m;
+        } else {
+            l = m + 1;
+        }
+    }
+    return nums[l];
+}
+
+console.log(FindMinRotatedSorted_BSLB([3,4,5,1,2]));
--- Blind75_Leetcode/Array/Find_Minimum_Rotated_Sorted/findMinRotatedSorted.js
+++ Blind75_Leetcode/Array/Find_Minimum_Rotated_Sorted/findMinRotatedSorted.js
@@ -33,3 +33,24 @@ var findMinRotatedSorted_BS = function(nums){
 }
 
 console.log(findMinRotatedSorted_BS([3,4,5,1,2]));
+
+
+
+//O(logn)
+//Solution: Binary Search (Lower Bound)
+var findMinRotatedSorted_BSLB = function(nums){
+    let l = 0;
+    let r = nums.length - 1;
+
+    while (l < r) {
+        let m = l + Math.floor((r - l)/2);
+        if (nums[m] < nums[r]) {
+            r = m;
+        } else {
+            l = m + 1;
+        }
+    }
+    return nums[l];
+}
+
+console.log(findMinRotatedSorted_BSLB([3,4,5,1,2]));
