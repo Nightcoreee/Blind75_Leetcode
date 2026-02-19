5798b77e273e016e4f4b6f47a3348bd17dec277c
Hà Đào <94346501+Nightcoreee@users.noreply.github.com>
2026-02-20 02:54:11 +0700

    More Solution for 3Sum

--- Blind75_Leetcode/Array/3Sum/ThreeSum.ts
+++ Blind75_Leetcode/Array/3Sum/ThreeSum.ts
@@ -30,3 +30,37 @@ function ThreeSum_HS(nums: number[]): number[][]{
 }
 
 console.log(ThreeSum_HS([-1,0,1,2,-1,-4]));
+
+
+//O(n)
+//Solution: Two Pointer
+function ThreeSum_TP(nums: number[]): number[][] {
+    nums.sort((a, b) => a - b);
+    const res = [];
+
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] > 0) break;
+        if(i > 0 && nums[i] === nums[i - 1]) continue;
+
+        let l = i + 1;
+        let r = nums.length - 1;
+        while(l < r) {
+            let sum = nums[i] + nums[l] + nums[r];
+            if(sum > 0) {
+                r--;
+            } else if(sum < 0) {
+                l++;
+            } else {
+                res.push([nums[i], nums[l], nums[r]]);
+                l++;
+                r--;
+                while(l < r && nums[l] === nums[l - 1]) {
+                    l++;
+                }
+            }
+        }
+    }
+    return res;
+}
+
+console.log(ThreeSum_HS([-1,1,0,-1,-4,2]));
